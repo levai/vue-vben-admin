02338cacb582de0af215adf2bed011233226696a
zlevai <zlevai@qq.com>
2025-12-30 18:12:55 +0800

    feat: 添加创建和更新时间字段的自动填充功能，更新SysDept和SysUser模型

--- apps/backend-service/src/main/java/com/vben/admin/model/entity/SysDept.java
+++ apps/backend-service/src/main/java/com/vben/admin/model/entity/SysDept.java
@@ -1,6 +1,8 @@
 package com.vben.admin.model.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -46,21 +48,25 @@ public class SysDept {
     /**
      * 创建人ID
      */
+    @TableField(fill = FieldFill.INSERT)
     private String createBy;
 
     /**
      * 创建时间
      */
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
     /**
      * 更新人ID
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private String updateBy;
 
     /**
      * 更新时间
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
 
     /**
